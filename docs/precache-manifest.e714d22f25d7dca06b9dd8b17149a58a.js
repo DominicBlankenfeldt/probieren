8d3050d45384b16e6e4efe7f21fb423cbdb40719
Dominic.Blankenfeldt <d.blankenfeldt@mbd-team.de>
2022-03-21 10:10:57 +0100

    pwa and run build

---
+++ docs/precache-manifest.e714d22f25d7dca06b9dd8b17149a58a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9e10d306fa43db52f9aacb9345e71cc",
+    "url": "/probieren/index.html"
+  },
+  {
+    "revision": "7dd5d3a6fa1a26815c92",
+    "url": "/probieren/js/about.2ca95ec1.js"
+  },
+  {
+    "revision": "672abb554a3f44f08199",
+    "url": "/probieren/js/app.5415747d.js"
+  },
+  {
+    "revision": "b8837ce8e6f43e030bab",
+    "url": "/probieren/js/chunk-vendors.87f50543.js"
+  },
+  {
+    "revision": "c603425452d64bbfe39db9e512b3d7be",
+    "url": "/probieren/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/probieren/robots.txt"
+  }
+]);
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  publicPath: "/probieren", //enter the name of the git repo here
+  outputDir: "./docs",
+  //pwa: {
+  //workboxPluginMode: "InjectManifest",
+  //workboxOptions: {
+  //swSrc: "src/service-worker.js",
+  //},
+  //},
+  css: {
+    extract: false,
+  },
+};
